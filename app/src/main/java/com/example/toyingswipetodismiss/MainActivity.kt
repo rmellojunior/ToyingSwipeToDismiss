e16f8171a4ee0c7f988e8a6f77f4be6e6ac142b5
rmellojunior <renatojuniorpt@gmail.com>
2019-05-03 14:47:09 +0100

    toying with multiple options

--- app/src/main/java/com/example/toyingswipetodismiss/MainActivity.kt
+++ app/src/main/java/com/example/toyingswipetodismiss/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.toyingswipetodismiss
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.recycler_view
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView.State
+import com.example.toyingswipetodismiss.SwipeHelperV2.UnderlayButton
+import java.util.LinkedList
+import com.example.toyingswipetodismiss.SwipeHelperV2.UnderlayButtonClickListener
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,12 +43,37 @@ class MainActivity : AppCompatActivity() {
     recycler_view.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
     recycler_view.adapter = adapter
 
-    val swipeController = SwipeHelper.setOnRightListener(object : SwipeCallback {
-      override fun onRightClicked(position: Int) {
-        adapter.list.removeAt(position)
-        adapter.notifyItemRemoved(position)
-        adapter.notifyItemRangeChanged(position, adapter.itemCount)
+//    val swipeController = SwipeHelper(object : SwipeCallback {
+////      override fun onRightClicked(position: Int) {
+////        adapter.list.removeAt(position)
+////        adapter.notifyItemRemoved(position)
+////        adapter.notifyItemRangeChanged(position, adapter.itemCount)
+////      }
+////    })
+////
+////    val itemTouchHelper = ItemTouchHelper(swipeController)
+////    itemTouchHelper.attachToRecyclerView(recycler_view)
+////
+////    recycler_view.addItemDecoration(object : RecyclerView.ItemDecoration() {
+////      override fun onDraw(c: Canvas, parent: RecyclerView, state: State) {
+////        swipeController.onDraw(c)
+////      }
+////    })
+
+    val swipeHelper = object : SwipeHelperV2(this, recycler_view) {
+      override fun instantiateUnderlayButton(viewHolder: RecyclerView.ViewHolder,
+        underlayButtons: MutableList<UnderlayButton>) {
+        underlayButtons.add(SwipeHelperV2.UnderlayButton(
+          "Delete",
+          0,
+          Color.parseColor("#FF3C30"),
+          object : SwipeHelperV2.UnderlayButtonClickListener {
+            override fun onClick(pos: Int) {
+              // TODO: onDelete
+            }
+          }
+        ))
       }
-    })
+    }
   }
 }
--- app/src/main/java/com/example/toyingswipetodismiss/SwipeHelper.kt
+++ app/src/main/java/com/example/toyingswipetodismiss/SwipeHelper.kt
@@ -1,12 +1,34 @@
 package com.example.toyingswipetodismiss
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import androidx.recyclerview.widget.ItemTouchHelper.Callback
 import androidx.recyclerview.widget.ItemTouchHelper.ACTION_STATE_IDLE
 import androidx.recyclerview.widget.ItemTouchHelper.LEFT
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import android.graphics.RectF
+import androidx.recyclerview.widget.ItemTouchHelper.ACTION_STATE_SWIPE
+import android.view.MotionEvent
+import android.view.View
 
-class SwipeHelper : Callback() {
+internal enum class ButtonsState {
+  GONE,
+  RIGHT_VISIBLE
+}
+
+class SwipeHelper(private val swipeCallback: SwipeCallback) : Callback() {
+
+  private var swipeBack = false
+
+  private var buttonShowedState = ButtonsState.GONE
+
+  private var currentItemViewHolder: RecyclerView.ViewHolder? = null
+
+  private lateinit var buttonInstance: RectF
+
+  private val buttonWidth = 300f
 
   override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: ViewHolder): Int {
     return makeMovementFlags(ACTION_STATE_IDLE, LEFT)
@@ -21,10 +43,122 @@ class SwipeHelper : Callback() {
     // callback for swipe to dismiss
   }
 
-  companion object {
-    fun setOnRightListener(swipeCallback: SwipeCallback) {
-
+  override fun convertToAbsoluteDirection(flags: Int, layoutDirection: Int): Int {
+    if (swipeBack) {
+      swipeBack = buttonShowedState !== ButtonsState.GONE
+      return 0
     }
+    return super.convertToAbsoluteDirection(flags, layoutDirection)
+  }
+
+  override fun onChildDraw(c: Canvas, recyclerView: RecyclerView,
+    viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int,
+    isCurrentlyActive: Boolean) {
+
+    if (actionState == ACTION_STATE_SWIPE) {
+      if (buttonShowedState != ButtonsState.GONE) {
+        val newDX = Math.min(dX, -300f)
+        super.onChildDraw(c, recyclerView, viewHolder, newDX, dY, actionState, isCurrentlyActive)
+      } else {
+        setTouchListener(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+      }
+    }
+
+    if (buttonShowedState == ButtonsState.GONE) {
+      super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+    }
+    currentItemViewHolder = viewHolder
+  }
+
+  private fun setTouchListener(c: Canvas, recyclerView: RecyclerView,
+    viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int,
+    isCurrentlyActive: Boolean) {
+    recyclerView.setOnTouchListener(object : View.OnTouchListener {
+      override fun onTouch(v: View, event: MotionEvent): Boolean {
+        swipeBack = event.action == MotionEvent.ACTION_CANCEL || event.action == MotionEvent.ACTION_UP
+        if (swipeBack) {
+          if (dX < -buttonWidth) {
+            buttonShowedState = ButtonsState.RIGHT_VISIBLE
+          }
+
+          if (buttonShowedState != ButtonsState.GONE) {
+            setTouchDownListener(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            setItemsClickable(recyclerView, false)
+          }
+        }
+        return false
+      }
+    })
+  }
+
+  private fun setTouchDownListener(c: Canvas, recyclerView: RecyclerView,
+    viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int,
+    isCurrentlyActive: Boolean) {
+    recyclerView.setOnTouchListener { v, event ->
+      if (event.action == MotionEvent.ACTION_DOWN) {
+        setTouchUpListener(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+      }
+      false
+    }
+  }
+
+  private fun setTouchUpListener(c: Canvas, recyclerView: RecyclerView,
+    viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int,
+    isCurrentlyActive: Boolean) {
+    recyclerView.setOnTouchListener { v, event ->
+      if (event.action === MotionEvent.ACTION_UP) {
+        super@SwipeHelper.onChildDraw(c, recyclerView, viewHolder, 0f, dY, actionState,
+          isCurrentlyActive)
+        recyclerView.setOnTouchListener { v, event -> false }
+        setItemsClickable(recyclerView, true)
+        swipeBack = false
+
+        if (swipeCallback != null && buttonInstance != null && buttonInstance.contains(event.x,
+            event.y)) {
+          swipeCallback.onRightClicked(viewHolder.adapterPosition)
+        }
+        buttonShowedState = ButtonsState.GONE
+        currentItemViewHolder = null
+      }
+      false
+    }
+  }
+
+  private fun setItemsClickable(recyclerView: RecyclerView, isClickable: Boolean) {
+    for (i in 0 until recyclerView.childCount) {
+      recyclerView.getChildAt(i).isClickable = isClickable
+    }
+  }
+
+  fun onDraw(c: Canvas) {
+    currentItemViewHolder?.let {
+      drawButtons(c, currentItemViewHolder!!)
+    }
+  }
+
+  private fun drawButtons(c: Canvas, viewHolder: RecyclerView.ViewHolder) {
+
+    val itemView = viewHolder.itemView
+    val p = Paint()
+
+    val rightButton = RectF(itemView.right.toFloat() - viewHolder.itemView.width, itemView.top.toFloat(),
+      itemView.right.toFloat(), itemView.bottom.toFloat())
+    p.color = Color.RED
+    c.drawRect(rightButton, p)
+    drawText("DELETE", c, rightButton, p)
+
+    buttonInstance = RectF(itemView.right.toFloat() - 300f, itemView.top.toFloat(),
+      itemView.right.toFloat(), itemView.bottom.toFloat())
+  }
+
+  private fun drawText(text: String, c: Canvas, button: RectF, p: Paint) {
+    val padding = 20f
+    val textSize = 60f
+    p.color = Color.WHITE
+    p.textSize = textSize
+
+    val textWidth = p.measureText(text)
+    c.drawText(text, c.width - buttonWidth - padding, button.centerY() + textSize / 2, p)
   }
 
 }
